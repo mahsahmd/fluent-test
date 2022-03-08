ceb65368d1685b9741e039601dfb5a55e626ab8e
mahsahmd <m.ahmadpoor@hami.blue>
2022-03-08 14:03:52 +0330

    fluent-ui-test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-loading-skeleton/dist/skeleton.css'
+import { Dropdown, initializeIcons, DropdownMenuItemType } from '@fluentui/react';
+
+initializeIcons();
+
 
 function App() {
+  const dropdownStyles = {
+    root: { width: 300 },
+    dropdownItem:{backgroundColor:'white', selectors:{
+      ':hover':{
+        color:"red"
+      }
+    }},
+    dropdownItemHeader: { backgroundColor: 'pink' },
+  };
+  const options = [
+    { key: 'fruitsHeader', text: 'Fruits', itemType: DropdownMenuItemType.Header },
+    { key: 'apple', text: 'Apple' },
+    { key: 'banana', text: 'Banana' },
+    { key: 'orange', text: 'Orange', disabled: true },
+    { key: 'grape', text: 'Grape' },
+    { key: 'divider_1', text: '-', itemType: DropdownMenuItemType.Divider },
+    { key: 'vegetablesHeader', text: 'Vegetables', itemType: DropdownMenuItemType.Header },
+    { key: 'broccoli', text: 'Broccoli' },
+    { key: 'carrot', text: 'Carrot' },
+    { key: 'lettuce', text: 'Lettuce' },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropdown
+        label="Disabled example with defaultSelectedKey"
+        defaultSelectedKey="broccoli"
+        options={options}
+        styles={dropdownStyles}
+      />
     </div>
   );
 }
